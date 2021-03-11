c2bc8fe44961854b501b72539b6fee197e84ccb6
Alessandro Puzielli <alessandro.puzielli@alepuzio.net>
2021-03-11 18:24:04 +0100

    now the words are in an unordered list

---
+++ src/main/java/it/javaboss/webapp/mms/ListImage.java
@@ -0,0 +1,18 @@
+package it.javaboss.webapp.mms;
+
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * @overview: this class formats the found images 
+ * */
+public class ListImage {
+
+	private List<String> list;
+	
+	public String format(){
+		String result = null;
+		Iterator<String> it = list.iterator();
+		return result;
+	}
+}
