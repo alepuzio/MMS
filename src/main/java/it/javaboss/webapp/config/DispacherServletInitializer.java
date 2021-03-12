7765642d8337048d608554f10253aedec0cad497
Alessandro Puzielli <alessandro.puzielli@alepuzio.net>
2021-03-12 09:26:13 +0100

    refactoring and comments

--- src/main/java/it/javaboss/webapp/config/DispacherServletInitializer.java
+++ src/main/java/it/javaboss/webapp/config/DispacherServletInitializer.java
@@ -9,16 +9,15 @@ import org.springframework.web.servlet.DispatcherServlet;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.templatemode.TemplateMode;
-import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
 
 @Controller
 public class DispacherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
-    private /*final*/ TemplateEngine templateEngine;
+	private TemplateEngine templateEngine;
 
 	@Override
 	protected Class<?>[] getRootConfigClasses() {
-		logger.info(">getRootConfigClasses");
+		logger.debug(">getRootConfigClasses");
 		return null;
 	}
 
@@ -26,61 +25,58 @@ public class DispacherServletInitializer extends AbstractAnnotationConfigDispatc
 	 * */
 	@Override
 	protected Class<?>[] getServletConfigClasses() {
-		logger.info(">getServletConfigClasses");
-		return new Class[]{
-				WebApplicationContextConfig.class
-		};
+		logger.debug(">getServletConfigClasses");
+		return new Class[] { WebApplicationContextConfig.class };
 	}
 
 	/**
 	 * 
 	 * It manages the request to our application
-	 * */
+	 */
 	@Override
 	protected String[] getServletMappings() {
-		logger.info(">getServletMappings");
-		return new String[]{"/"};
+		logger.debug(">getServletMappings");
+		return new String[] { "/" };
 	}
 
 	/**
 	 * It works as web.xml
-	 * */
-    @Override
-    public void onStartup(ServletContext servletContext) {
-		logger.info(">onStartup");
-        // Load Spring web application configuration
-        AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
-        context.register(WebApplicationContextConfig.class);
-        // Create and register the DispatcherServlet
-        ServletRegistration.Dynamic registration = servletContext.addServlet("mms", new DispatcherServlet(context));
-        registration.setLoadOnStartup(1);
-        registration.addMapping("/mms/*");
-        
-         this.templateEngine = new TemplateEngine();
-        this.templateEngine.setTemplateResolver(templateResolver(servletContext));
-    }
-    
+	 */
+	@Override
+	public void onStartup(ServletContext servletContext) {
+		logger.debug(">onStartup");
+		// Load Spring web application configuration
+		AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
+		context.register(WebApplicationContextConfig.class);
+		// Create and register the DispatcherServlet
+		ServletRegistration.Dynamic registration = servletContext.addServlet("mms", new DispatcherServlet(context));
+		registration.setLoadOnStartup(1);
+		registration.addMapping("/mms/*");
 
-	 public ServletContextTemplateResolver templateResolver(ServletContext servletContext) {
-	        //ClassLoaderTemplateResolver configurer = new ClassLoaderTemplateResolver();
-	        ServletContextTemplateResolver templateResolver = new ServletContextTemplateResolver(servletContext);
-	        //templateResolver.setPrefix("/WEB-INF/templates/");
-	        
-	        templateResolver.setPrefix("/templates/");
-	        templateResolver.setSuffix(".html");//o jps
-	        templateResolver.setTemplateMode(TemplateMode.HTML);
-	        templateResolver.setCharacterEncoding("UTF-8");
-	        templateResolver.setOrder(0);  // this is important. This way spring //boot will listen to both places 0 and 1
-	        templateResolver.setCacheable(false);
-	        templateResolver.setCheckExistence(true);
-	        templateResolver.setCacheTTLMs(Long.valueOf(3600000L));
-	        
-	        // Cache is set to true by default. Set to false if you want templates to
-	        // be automatically updated when modified.
-	        templateResolver.setCacheable(true);
-	        
-	        return templateResolver;
-	    }
+		this.templateEngine = new TemplateEngine();
+		this.templateEngine.setTemplateResolver(templateResolver(servletContext));
+	}
 
-    
+	public ServletContextTemplateResolver templateResolver(ServletContext servletContext) {
+		ServletContextTemplateResolver templateResolver = new ServletContextTemplateResolver(servletContext);
+		/*
+		 * where is the directory with the files to render
+		 * */
+		templateResolver.setPrefix("/templates/");
+		templateResolver.setSuffix(".html");
+		templateResolver.setTemplateMode(TemplateMode.HTML);
+		templateResolver.setCharacterEncoding("UTF-8");
+		/* this is important. This way spring
+		 boot will listen to both places 0 and 1*/
+		templateResolver.setOrder(0); 
+		templateResolver.setCheckExistence(true);
+		templateResolver.setCacheTTLMs(Long.valueOf(3600000L));
+
+		/* Cache is set to true by default. Set to false if you want templates
+		 to be automatically updated when modified.*/
+		templateResolver.setCacheable(false);
+		
+		return templateResolver;
+	}
+
 }
