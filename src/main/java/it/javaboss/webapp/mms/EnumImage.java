a88b95b887eabeba5346165a154aad33e874b903
Alessandro Puzielli <alessandro.puzielli@alepuzio.net>
2021-03-12 09:54:45 +0100

    from 10 to 30

--- src/main/java/it/javaboss/webapp/mms/EnumImage.java
+++ src/main/java/it/javaboss/webapp/mms/EnumImage.java
@@ -1,52 +1,40 @@
+
+
+
 package it.javaboss.webapp.mms;
 
 public enum EnumImage {
 
+	DIECI(10,"TZ","TAZZE"),
 	UNDICI(11,"DD","DADI"),
+	DODICI(12,"DN","DNA"),
+	TREDICI(13,"DM","DAMA"),
+	QUATTORDICI(14,"TR","TORRE"),
+	QUINDI(15,"DL","DUELLO"),
+	SEDICI(16,"DC","DUCE"),
+	DICIASETTE(17,"DGH","DIGA"),
+	DICIOTTO(18,"TF","TUFFO"),
+	DICIANNOVE(19,"TP","TAPPI"),
+	VENTI(20,"NS","NASO"),
+	VENTIUNO(21,"ND","NUDO"),
+	VENTIDUE(22,"NN","NONNA"),
+	VENTITRE(23,"NM","ANIMA"),
 	VENTIQUATTRO(24,"NR","NERO"),
+	VENTICINQUE(25,"NL","ANELLO"),
+	VENTISEI(26,"NC","NOCI"),
+	VENTISETTE(27,"GNCH","GNOCCHI"),
+	VENTIOTTO(28," NV"," NEVE"),
+	VENTINOVE(29,"NB","NUBI"),
+	TRENTA(30," MS"," MASSAIA"),
+
+	
+	
 	CENTO(00,"SASSO", "SASSO")
 	
 	
 	;
 /*
-	12,DN,DNA
-
-	13,DM,DAMA
-
-	14,TR,TORRE
-
-	15,DL,DUELLO
-
-	16,DC,DUCE
-
-	17,DGH,DIGA
-
-	18,TF,TUFFO
-
-	19,TP,TAPPI
-
-	20,NS,NASO
-
-
-	21,ND,NUDO
-
-	22,NN,NONNA
-
-	23,NM,ANIMA
-
-	
-
-	25,NL,ANELLO
-
-	26,NC,NOCI
-
-	27,GNCH,GNOCCHI
-	28, NV, NEVE
-
-	29,NB,NUBI
-
-	30, MS, MASSAIA
-*/
+	*/
 	private int number;
 	private String phonetic;
 	private String image;
@@ -69,3 +57,4 @@ public enum EnumImage {
 	
 	
 }
+
